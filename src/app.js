3f7268c3ee856b8755cd6fa1316c9ae90fc6d978
TetianaBut <tbutok54@gmail.com>
2023-09-02 12:04:22 +0200

    Display the temperature data using the API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,57 @@
-// set Unit Temperature
+// input now  Date
+function displaytDate() {
+  let nowDate = new Date();
+  // console.log(nowDate);
+  let hours = nowDate.getHours();
+  let minutes = nowDate.getMinutes();
+  let day = nowDate.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${days[day]}  ${hours}:${minutes}`;
+}
+
+// display Weather Condition
 function displayWeatherCondition(response) {
   console.log(response.data);
+  let displayCity = document.querySelector("h1");
+  let displayDate = document.querySelector("#input-time");
+  let displayDescript = document.querySelector("#description");
+  let displayIcons = document.querySelector("#weather-icon");
+  let displayTemp = document.querySelector("#setTempValue");
+  let displayFeelsLlike = document.querySelector("#FeelsLlike");
+  let displayHumidity = document.querySelector("#humidity");
+  let displayWind = document.querySelector("#wind");
+  displayCity.innerHTML = `${response.data.city} ${response.data.country}`;
+  // displayDate.innerHTML = displaytDate(response.data.time * 1000);
+  displayDate.innerHTML = displaytDate(response.data.time);
+  displayDescript.innerHTML = `${response.data.condition.description}`;
+  displayIcons.setAttribute("src", `${response.data.condition.icon_url}`);
+  displayIcons.setAttribute("alt", `${response.data.condition.icon}`);
+  currentTemp = response.data.temperature.current;
+  displayTemp.innerHTML = `${Math.round(currentTemp)}`;
+  currentFeelsLlike = response.data.temperature.feels_like;
+  displayFeelsLlike.innerHTML = `${Math.round(currentFeelsLlike)}`;
+  displayHumidity.innerHTML = ` ${response.data.temperature.humidity}`;
+  currentWind = response.data.wind.speed;
+  //   if (unit === "metric") {
+  //     currentWind = currentWind * 3.6;
+  //   }
+  displayWind.innerHTML = ` ${Math.round(currentWind)}${unitWind}`;
+  positionCityName = `lat=${response.data.coordinates.latitude}&lon=${response.data.coordinates.longitude}`;
+  console.log(positionCityName);
 }
 
 function retrieveDataWeather(cityName) {
@@ -12,6 +63,7 @@ function retrieveDataWeather(cityName) {
 
 // start
 let currentTemp = 0;
+let currentFeelsLlike = 0;
 let currentWind = 0;
 let unitWind = "km/h";
 let unit = "metric";
